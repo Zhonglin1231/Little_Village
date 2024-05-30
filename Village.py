8092303086d5cf3e90c0a592f273e495b57230a0
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2024-05-30 22:47:12 +0800

    add the reproduction system to village.py
    red is the new born kid, but the issue is, why the number of people decreases in the end?

--- Village.py
+++ Village.py
@@ -6,7 +6,7 @@ pygame.init()
 
 WIDTH = 1000
 HEIGHT = 800
-SCREEN =pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 max_speed = 1
 num_of_people = 100
 map_path = "./resource/map_HongKong.png"
@@ -14,11 +14,8 @@ sex_set = ('male', 'female')
 occupation_set = ('worker', 'farmer', 'leader', 'soldier')
 skills_set = ('building', 'farming', 'fighting', 'leading')
 special_status_set = ('sick', 'injured', 'pregnant')
-nationality_set = ('Chinese', 'American', 'British')
+nationality_set = ('Chinese', 'American', 'British', 'new')
 
-STATE_COUNTS = {
-    "dead": 0
-}
 
 COLOR_DEFINITIONS = {
     "grey": (35, 35, 40),
@@ -31,13 +28,15 @@ COLOR_DEFINITIONS = {
 
 COLORS = {
     "background": COLOR_DEFINITIONS["grey"],
-    None: COLOR_DEFINITIONS["light_grey"]
+    "info": COLOR_DEFINITIONS["white"],
+    None: COLOR_DEFINITIONS["light_grey"],
+    "new": COLOR_DEFINITIONS["red"]
 }
 
 class Person:
-    def __init__(self):
-        self.x = random.randint(0, WIDTH)
-        self.y = random.randint(0, HEIGHT)
+    def __init__(self, x=None, y=None, nationality=None):
+        self.x = x if x is not None else random.randint(0, WIDTH)
+        self.y = y if y is not None else random.randint(0, HEIGHT)
         self.dx = 0
         self.dy = 0
         self.health = 100
@@ -47,21 +46,20 @@ class Person:
         self.occupation = random.choice(occupation_set)
         self.skills = random.choice(skills_set)
         self.special_status = None  # e.g., 'sick', 'injured'
-        self.nationality = None
+        self.nationality = nationality if nationality is not None else None
         self.IQ = random.randint(60, 180)
         self.intelligence = random.randint(0, 100)
+        self.last_reproduce = None
 
-
-    def show(self, size = 10):
+    def show(self, size=10):
         pygame.draw.circle(SCREEN, COLORS[self.nationality], (self.x, self.y), size)
 
-    def move(self, speed = 0.75):
-
+    def move(self, speed=0.75):
         # adjust position vector
         self.x += self.dx
         self.y += self.dy
 
-        # aviod going out of bounds
+        # avoid going out of bounds
         if self.x >= WIDTH:
             self.x = WIDTH - 1
             self.dx *= -1
@@ -98,9 +96,9 @@ class Person:
         self.happiness = max(0, min(self.happiness, 100))
 
     def age_up(self):
-        self.age += 1
+        self.age += 1/365
         if self.age > 80:
-            self.change_health(-10)
+            self.change_health(-0.1)
 
     def work(self):
         if self.occupation == "worker":
@@ -111,9 +109,13 @@ class Person:
             print("farmer")
 
     # -----------------------Interact with other people-------------------
-    def reproduction(self, other):
-        # to be added
-        print("reproduction")
+    def reproduction(self):
+        self.health -= 30
+
+        # create a new person and add to the population
+        new_person = Person(x=self.x, y=self.y, nationality="new")
+        village.population.append(new_person)
+        village.INFO["Population Size"] += 1
 
     # -----------------------get stats------------------------------------
     def get_status(self):
@@ -125,23 +127,44 @@ class Person:
             'occupation': self.occupation,
             'skills': self.skills,
             'special_status': self.special_status,
-            'nationality': self.national,
+            'nationality': self.nationality,
             'IQ': self.IQ,
             'intelligence': self.intelligence
-    }
+        }
 
 
-# class Village:
-    # may be consider to add?
+class Village:
+    def __init__(self, num_of_people):
+        self.population = [Person() for _ in range(num_of_people)]
+        self.INFO = {
+            "Population Size": num_of_people,
+            "GDP": 0,
+            "dead": 0,
+            "year": 0
+        }
+
+    def stimulate_day(self):
+        # update data
+        self.INFO["year"] += 1/365
+        self.INFO["year"] = round(self.INFO["year"], 3)
+        self.show_stats()
+
+    def show_stats(self):
+        # show stats
+        for i, (info, count) in enumerate(self.INFO.items()):
+            text = f"{info}: {count}"
+            font = pygame.font.Font(None, 36)
+            text = font.render(text, True, COLORS["info"])
+            SCREEN.blit(text, (10, 10 + 40 * i))
 
 
-# initialize people
-people = [Person() for i in range(num_of_people)]
+# initialize village
+village = Village(100)
+print(village.population)
 
 # pygame loop
 animating = True
 while animating:
-
     # set background color
     SCREEN.fill(COLORS["background"])
     # # add the map on to the background
@@ -149,28 +172,64 @@ while animating:
     # SCREEN.blit(map, (0, 0))
 
     # pygame draws things to the screen
-    for p in people:
-        # random movement
+    village.stimulate_day()
+
+    # people movement
+    for p in village.population:
+        p.age_up()
+        p.work()
         p.move()
         p.show()
 
     # do actions for people
-    for p in people:
-        print("some actions")
+    for p in village.population:
+
+        if p.health <= 0:
+            village.population.remove(p)
+            village.INFO["Population Size"] -= 1
+            village.INFO["dead"] += 1
+        # the action with others
+        for other in village.population:
+            # make sure distance is close to interact
+            if math.sqrt((p.x - other.x)**2 + (p.y - other.y)**2) < 5 and p != other:
+                # action for high health people
+                if p.health > 60 and p.age >= 18 and other.age >= 18 and p.age <= 60 and other.age <= 60:
+                    # action for female
+                    if p.sex == "female":
+                        # action for diff gender
+                        if other.sex == "male":
+                            # reproduction
+                            # possibility of 0.1 to reproduce and pass 1 year didn't reproduce
+                            if p.last_reproduce is not None:
+                                if random.random() < 0.1 and village.INFO["year"] - p.last_reproduce > 1:
+                                    p.reproduction()
+                                    p.last_reproduce = village.INFO["year"]
+                                    # possibility of 0.01 to die after reproduce
+                                    if random.random() < 0.01:
+                                        p.change_health(-100)
+                                        village.INFO["dead"] += 1
+                            if p.last_reproduce is None:
+                                random.random() < 0.1
+                                p.reproduction()
+                                p.last_reproduce = village.INFO["year"]
+                                # possibility of 0.01 to die after reproduce
+                                if random.random() < 0.01:
+                                    p.change_health(-100)
+                                    village.INFO["dead"] += 1
+
+
 
     # update the display
     pygame.display.flip()
 
     # track user interaction
     for event in pygame.event.get():
-
         # user closes the window
         if event.type == pygame.QUIT:
             animating = False
 
         # users presses a key
         if event.type == pygame.KEYDOWN:
-            
             # escape key closes the animation
             if event.key == pygame.K_ESCAPE:
                 animating = False
