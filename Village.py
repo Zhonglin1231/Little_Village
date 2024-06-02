bb2e7909413b2d741bf4964599573f9666a08891
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2024-06-02 21:27:26 +0800

    Visual Village.py
    * Visualize the population information
    * Fix the bug that population isn't growing

--- Village.py
+++ Village.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from collections import defaultdict, deque
 
 pygame.init()
 
@@ -9,6 +12,7 @@ HEIGHT = 800
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 max_speed = 1
 num_of_people = 100
+grid_size = 50
 map_path = "./resource/map_HongKong.png"
 sex_set = ('male', 'female')
 occupation_set = ('worker', 'farmer', 'leader', 'soldier')
@@ -102,12 +106,13 @@ class Person:
             self.change_health(-0.1)
 
     def work(self):
-        if self.occupation == "worker":
-            # to be added
-            print("worker")
-        elif self.occupation == "farmer":
-            # to be added
-            print("farmer")
+        # if self.occupation == "worker":
+        #     # to be added
+        #     print("worker")
+        # elif self.occupation == "farmer":
+        #     # to be added
+        #     print("farmer")
+        return
 
     # -----------------------Interact with other people-------------------
     def reproduction(self):
@@ -142,17 +147,36 @@ class Person:
 class Village:
     def __init__(self, num_of_people):
         self.population = [Person() for _ in range(num_of_people)]
+        self.grid = defaultdict(list)
         self.INFO = {
             "Population Size": num_of_people,
             "GDP": 0,
             "dead": 0,
-            "year": 0
+            "year": 0,
+            "population change": 0
         }
+
+    def update_grid(self):
+        self.grid = defaultdict(list)
+        for person in self.population:
+            grid_x = person.x // grid_size
+            grid_y = person.y // grid_size
+            self.grid[(grid_x, grid_y)].append(person)
+
+    def get_nearby_people(self, person):
+        grid_x = person.x // grid_size
+        grid_y = person.y // grid_size
+        nearby_people = []
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                nearby_people.extend(self.grid[(grid_x + dx, grid_y + dy)])
+        return nearby_people
 
     def stimulate_day(self):
         # update data
         self.INFO["year"] += 1/365
         self.INFO["year"] = round(self.INFO["year"], 3)
+        self.update_grid()
         self.show_stats()
 
     def show_stats(self):
@@ -164,13 +188,53 @@ class Village:
             SCREEN.blit(text, (10, 10 + 40 * i))
 
 
+class DynamicChart:
+
+    def __init__(self) :
+        self.fig, self.ax = plt.subplots()
+        self.x_data = deque(maxlen=1000)
+        self.y_data = deque(maxlen=1000)
+        self.line, = plt.plot([], [], 'r-', animated=True)
+
+    def create_chart(self):
+        self.ax.set_xlim(0, 50)
+        self.ax.set_ylim(50, 800)
+        return self.line,
+
+    def update(self, frame):
+        self.x_data.append(village.INFO["year"])
+        self.y_data.append(village.INFO["Population Size"])
+        self.line.set_data(self.x_data, self.y_data)
+        return self.line,
+
+    def show(self):
+        plt.show(block=False)
+
+
 # initialize village
 village = Village(100)
-print(village.population)
+current_year_info = dict(village.INFO)
+last_year_info = None
 
+# create dynamic chart
+chart = DynamicChart()
+anim = animation.FuncAnimation(chart.fig, chart.update, frames=range(200), init_func=chart.create_chart, blit=True)
+chart.show()
+ 
 # pygame loop
 animating = True
 while animating:
+    # parameter calculation part
+        # calculate yearly rate
+    year = f"{village.INFO['year']:.3f}"
+    if year[-3:] in ["000", "999", "001"]:
+        last_year_info = current_year_info
+        current_year_info = dict(village.INFO)
+        print(current_year_info["Population Size"], last_year_info["Population Size"])
+        # calculate change of population
+        village.INFO["population change"] = current_year_info["Population Size"] - last_year_info["Population Size"]
+        print(village.INFO["population change"])
+
     # set background color
     SCREEN.fill(COLORS["background"])
     # # add the map on to the background
@@ -194,10 +258,12 @@ while animating:
             village.population.remove(p)
             village.INFO["Population Size"] -= 1
             village.INFO["dead"] += 1
+
         # the action with others
-        for other in village.population:
+        nearby_people = village.get_nearby_people(p)
+        for other in nearby_people:
             # make sure distance is close to interact
-            if math.sqrt((p.x - other.x)**2 + (p.y - other.y)**2) < 5 and p != other:
+            if math.sqrt((p.x - other.x)**2 + (p.y - other.y)**2) < 20 and p != other:
                 # action for high health people
                 if p.health > 60 and p.age >= 18 and other.age >= 18 and p.age <= 60 and other.age <= 60:
                     # action for female
@@ -223,9 +289,12 @@ while animating:
                                     p.change_health(-100)
                                     village.INFO["dead"] += 1
 
+        p.health += 0.1
+
 
 
     # update the display
+    # plt.pause(0.01)
     pygame.display.flip()
 
     # track user interaction
@@ -239,3 +308,6 @@ while animating:
             # escape key closes the animation
             if event.key == pygame.K_ESCAPE:
                 animating = False
+
+pygame.quit()
+plt.close()
